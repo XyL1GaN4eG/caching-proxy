ac1322b0f1c15aab53ea740777262bc378f96f73
XyL1GaN4eG <trolloguy@mail.ru>
2026-05-30 22:33:43 +0300

    implement cache.go

---
+++ internal/cache/cache.go
@@ -0,0 +1,51 @@
+package cache
+
+import (
+	"net/http"
+	"sync"
+)
+
+/*
+todo:
+ improve key-generating (for auth-security)
+ add timeout and ttl for keys
+ add lru
+*/
+
+type Cache struct {
+	mu sync.RWMutex
+	m  map[string]CachedResponse
+}
+
+type CachedResponse struct {
+	code   int
+	header http.Header
+	body   []byte
+}
+
+func NewCache() *Cache {
+	return &Cache{m: make(map[string]CachedResponse)}
+}
+
+func key(r *http.Request) string {
+	return r.Method + "|" + r.URL.Path + "?" + r.URL.RawQuery
+}
+
+func (c *Cache) Get(r *http.Request) (res CachedResponse, ok bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	res, ok = c.m[key(r)]
+	return res, ok
+}
+
+func (c *Cache) Set(req *http.Request, res *http.Response, body []byte) (cr CachedResponse) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	cr = CachedResponse{
+		code:   res.StatusCode,
+		header: res.Header.Clone(),
+		body:   body,
+	}
+	c.m[key(req)] = cr
+	return cr
+}
